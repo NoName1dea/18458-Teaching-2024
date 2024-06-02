44b00d0d08a5464b1e5db203a54d05d8b40b5a93
Joel Yowell <joelyowell9@gmail.com>
2024-06-02 16:44:26 -0500

    Done

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teaching/Joel/Drivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teaching/Joel/Drivetrain.java
@@ -1,6 +1,28 @@
 package org.firstinspires.ftc.teamcode.Teaching.Joel;
 
 
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
 public class Drivetrain {
+    public final DcMotor leftRear, leftFront, rightFront, rightRear;
+    public final Gamepad gamepad1;
+    public HardwareMap hardwareMap;
+    public Telemetry telemetry;
+    public Drivetrain(OpMode opMode) {
+
+        this.gamepad1 = opMode.gamepad1;
+        this.hardwareMap = opMode.hardwareMap;
+        this.telemetry = opMode.telemetry;
+
+        leftFront = (DcMotor) hardwareMap.get("leftFront");
+        leftRear = (DcMotor) hardwareMap.get("leftRear");
+        rightFront = (DcMotor) hardwareMap.get("rightFront");
+        rightRear = (DcMotor) hardwareMap.get("rightRear");
+    }
 
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teaching/Joel/PLEASE.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teaching/Joel/PLEASE.java
@@ -1,5 +1,20 @@
 package org.firstinspires.ftc.teamcode.Teaching.Joel;
 
-public class PLEASE {
-    int number = 7;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class PLEASE extends LinearOpMode {
+    public DcMotor motor;
+    public Servo servo;
+    public Gamepad gamepad;
+    @Override
+    public void runOpMode() throws InterruptedException {
+        while (opModeIsActive()) {
+            if (gamepad1.cross) {
+                motor.setPower(0);
+            }
+        }
+    }
 }
