70d336fad8cfac776eedf47ec5f783dedc4b936e
Joel Yowell <joelyowell9@gmail.com>
2024-06-06 14:23:58 -0500

    GOSH DARNIT

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Commandbase/Drivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Commandbase/Drivetrain.java
@@ -1,4 +1,9 @@
 package org.firstinspires.ftc.teamcode.Commandbase;
 
+import com.acmerobotics.dashboard.config.Config;
+
+@Config
 public class Drivetrain {
+    int number = 1;
+    double number2 = 1.0;
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teaching/Leo/Slides.java
@@ -0,0 +1,4 @@
+package org.firstinspires.ftc.teamcode.Teaching.Leo;
+
+public class Slides {
+}
